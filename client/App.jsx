54209204e0128ad71a32438f2a115313c94770b8
Derek <dk.derek.koh@gmail.com>
2023-07-14 20:00:36 -0500

    initial set up

---
+++ client/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './styles/styles.scss';
+import { TestContainer } from './containers/TestContainer';
+
+export function App() {
+  return (
+    <TestContainer />
+    // <Routes>
+    //   <Route />
+    //   <Route />
+    //   <Route />
+    // </Routes>
+  );
+}
---
+++ client/containers/TestContainer.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function TestContainer() {
+  return <div>Testing to see if this actually loads</div>;
+}
