f28df488426d96e17258829fc3bc84f2567e5e7a
Adrian Zywno <adria@MacBook-Air.local>
2023-07-15 16:42:39 -0400

    First commit for userConroller

---
+++ server/controller/userController.js
@@ -0,0 +1,56 @@
+const db = require('../models/buddyModel');
+
+const userController = {};
+
+
+//? getUsers method to  retrieve user info from db
+userController.getUsers = (req, res, next) => {
+    
+    const queryString = 'SELECT id, username, password, name FROM users WHERE username = $1';
+
+    const username = req.params.username;
+
+    db.query(queryString, username)
+        .then((data) => {
+            res.locals.user = data.rows[0];
+
+            return next();
+
+        }).catch((err) => {
+            return (
+                next({
+                    status: 400,
+                    log: `${err}: Error on user.Controller.getUsers`,
+                    message: 'Error getting user'
+                })
+            )
+        })
+}
+
+userController.createUser = (req, res, next) => {
+        
+    const queryString = 'INSERT INTO users (id, username, password, name, profilepic) FROM users VALUES ($1, $2, $3, $4)';
+    
+    const { username, password, name, profilepic } = req.params;
+
+    const values = [ username, password, name, profilepic ];
+
+
+    db.query(queryString, values)
+        .then((data) => {
+            res.locals.user = data.rows[0];
+
+            return next();
+            
+        }).catch((err) => {
+            return (
+                next({
+                    status: 400,
+                    log: `${err}: Error on user.Controller.getUsers`,
+                    message: 'Error getting user'
+                })
+            )
+        })
+}
+
+module.exports = userController;
