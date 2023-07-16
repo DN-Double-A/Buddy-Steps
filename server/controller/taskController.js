4f34bc3d6c794ffad20ab9bbdc5b2a47958e659e
Derek <dk.derek.koh@gmail.com>
2023-07-16 13:11:53 -0500

    Added full functionality for updateTask controller.
    also adjusted the naming convention to be createTask and updateTask to follow proper naming flow.

--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -6,9 +6,8 @@ const taskController = {};
 
 // Modular error creator:
 const createErr = (errInfo) => {
-  const { method, type, err } = errInfo;
   return {
-    log: error,
+    log: errInfo,
     message: {
       err: `taskController error: Incorrect data received.`,
     },
@@ -33,14 +32,14 @@ taskController.getTaskData = async function (req, res, next) {
     res.locals.taskData = result.rows;
     return next();
   } catch (error) {
-    const newErr = createErr({ error });
-    return next(error);
+    const newErr = createErr(error);
+    return next(newErr);
   }
 };
 
 //add the task to the task to table
 //link the userid and the task in the task in the table.
-taskController.createData = async function (req, res, next) {
+taskController.createTask = async function (req, res, next) {
   try {
     // destructuring the task, startDate and endDate from req.body
     const { task, startDate, endDate } = req.body;
@@ -48,7 +47,8 @@ taskController.createData = async function (req, res, next) {
 
     // get the user id from the users table
     const foundUser = await db.query(
-      `SELECT id FROM users WHERE username='${currentUser}'`
+      `SELECT id FROM users WHERE username= $1;`,
+      [currentUser]
     );
     const userId = foundUser.rows[0].id;
 
@@ -74,11 +74,79 @@ taskController.createData = async function (req, res, next) {
       `,
       values: [userId, newTaskId],
     };
-
+    res.locals.newTaskId = newTaskId;
     await db.query(joinTableQuery);
     return next();
   } catch (error) {
-    return next(error);
+    const newErr = createErr(error);
+    return next(newErr);
+  }
+};
+
+taskController.updateTask = async function (req, res, next) {
+  try {
+    // pulling username from the query parameter in the url.
+    const taskIdToBeUpdated = req.query.taskId;
+
+    // pulling task from
+    const updatedTask = req.body.updatedTask;
+    const updatedEndTime = req.body.updatedEndTime;
+    let updatedId;
+
+    // query to update the task specific to that user.
+    if (!updatedEndTime) {
+      const updateTaskQuery = `
+        UPDATE tasks
+        SET task = $1
+        WHERE id = $2
+        RETURNING id;
+      `;
+
+      // assigning the updated task id to variable updated once done.
+      updatedId = await db.query(updateTaskQuery, [
+        updatedTask,
+        taskIdToBeUpdated,
+      ]);
+
+      // query to update the end time specific to that user.
+    } else if (!updatedTask) {
+      const updateTimeQuery = `
+        UPDATE tasks
+        SET enddate = $1
+        WHERE id = $2
+        RETURNING id;
+      `;
+
+      // assigning the updated task id to variable updated once done.
+      updatedId = await db.query(updateTimeQuery, [
+        updatedEndTime,
+        taskIdToBeUpdated,
+      ]);
+
+      // query to update both task name and end time specific to that user.
+    } else if (updatedTask && updatedEndTime) {
+      const updateTaskAndTimeQuery = `
+        UPDATE tasks
+        SET task = $1, enddate = $2 
+        WHERE id = $3
+        RETURNING id;
+      `;
+
+      // assigning the updated task id to variable updated once done.
+      updatedId = await db.query(updateTaskAndTimeQuery, [
+        updatedTask,
+        updatedEndTime,
+        taskIdToBeUpdated,
+      ]);
+    }
+
+    // sending back updated task id to confirm it was updated.
+    res.locals.updatedTaskId = updatedId.rows[0].id;
+    console.log('updatedId: ', updatedId);
+    return next();
+  } catch (error) {
+    const newErr = createErr(error);
+    return next(newErr);
   }
 };
 
