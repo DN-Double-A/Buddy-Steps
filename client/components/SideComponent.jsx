636b5ced7766340a7997be18684c5ba5d4cfbef2
Derek <dk.derek.koh@gmail.com>
2023-07-16 21:45:15 -0500

    added side container and component

---
+++ client/components/SideComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function SideComponent() {
+  <>
+    <p>hello</p>
+  </>;
+}
---
+++ client/containers/SideContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SideComponent } from '../components/SideComponent';
+
+export function SideContainer() {
+  <div className="main-side-container">
+    <SideComponent />
+  </div>;
+}
